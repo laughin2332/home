3d1fffcbfe72ca40881e8e31b562604d4661353c
らーふぃん <99650625+laughin2332@users.noreply.github.com>
2023-01-22 20:59:00 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+import $ from "https://cdn.skypack.dev/jquery@3.6.3";
+$(function(){
+    $(".btn-gnavi").on("click", function(){
+        // ハンバーガーメニューの位置を設定
+        var rightVal = 0;
+        if($(this).hasClass("open")) {
+            // 位置を移動させメニューを開いた状態にする
+            rightVal = -300;
+            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+            $(this).removeClass("open");
+        } else {
+            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+            $(this).addClass("open");
+        }
+ 
+        $("#global-navi").stop().animate({
+            right: rightVal
+        }, 200);
+    });
+});
